cf26ce2c3aa52be47d532eb6b24760d1f556ab5a
L.R.B.T <luisrafaelbernaltorres@Maclaus2.local>
2019-12-07 11:10:34 +0100

    initial commit

---
+++ rest_api_project/src/Controller/UsingApiController.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsingApiController
+{
+    /*private $customerRepository;
+
+    public function __construct(CustomerRepository $customerRepository)
+    {
+        $this->customerRepository = $customerRepository;
+    }*/		
+
+    /**
+     * @Route("/question/", name="question_get", methods={"GET"})
+     */
+    public function readcsv(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $firstName = $data['firstName'];
+        $lastName = $data['lastName'];
+        $email = $data['email'];
+        $phoneNumber = $data['phoneNumber'];
+
+        if (empty($firstName) || empty($lastName) || empty($email) || empty($phoneNumber)) {
+            throw new NotFoundHttpException('Expecting mandatory parameters!');
+        }
+
+        $this->customerRepository->saveCustomer($firstName, $lastName, $email, $phoneNumber);
+
+        return new JsonResponse(['status' => 'Customer created!'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/question/", name="question_post", methods={"POST"})
+     */
+        public function readjson(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $firstName = $data['firstName'];
+        $lastName = $data['lastName'];
+        $email = $data['email'];
+        $phoneNumber = $data['phoneNumber'];
+
+        if (empty($firstName) || empty($lastName) || empty($email) || empty($phoneNumber)) {
+            throw new NotFoundHttpException('Expecting mandatory parameters!');
+        }
+
+        $this->customerRepository->saveCustomer($firstName, $lastName, $email, $phoneNumber);
+
+        return new JsonResponse(['status' => 'Customer created!'], Response::HTTP_CREATED);
+    }
+}
