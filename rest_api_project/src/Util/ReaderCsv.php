74e356d4e15f5013544f1953990b3327431e241f
Luis R. Bernal Torres <luisrafaelbernaltorres@Maclaus2.local>
2019-12-07 18:11:51 +0100

    final commit

--- rest_api_project/src/Util/ReaderCsv.php
+++ rest_api_project/src/Util/ReaderCsv.php
@@ -1,4 +1,5 @@
 <?php
+// adding with command composer require league/csv:^8.0
 
 namespace App\Util;
 use App\Entity\QuestionList;
@@ -6,12 +7,71 @@ use App\Entity\Question;
 use App\Entity\Choice;
 use Doctrine\ORM\EntityManagerInterface;
 use League\Csv\Reader;
-
 //needs adding the libraries  wich permits charging the csv files
 // parameters wich will use are the file name 
 
 class ReaderCsv{
+	/**
+     * Configure
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
+     */
+	 protected function configure()
+    {
+        $this
+            ->setName('csv:import')
+            ->setDescription('Imports the CSV data file')
+        ;	
+    }
+
+
+//TODO needs implemneting all the retriving data
+//
+
+
 	public function csvcread($value){
-		return str_rot13($value);
+
+		        $reader = Reader::createFromPath($value);
+		        $results = $reader->fetchAssoc();
+
+
+		        foreach ($results as $row) {
+
+               // create new athlete
+               $athlete = (new QuestionList())
+               	 ->setFirstName($row['first_name'])
+               	 ->setLastName($row['last_name'])
+               	 ->setDateOfBirth(new \DateTime($row['date_of_birth']))
+               	 ->setWeight($row['weight'])
+              ;
+
+                $this->em->persist($athlete);
+
+	            // create new Competitor
+    	        $competitor = (new Competitor())
+        	     ->setCategory($row['category'])
+         		 ->setCompetition($row['competition'])
+              ;
+
+            	$this->em->persist($competitor);
+
+            	$athlete->setCompetitor($competitor);
+        		}
+
+        		$this->em->flush();
+
+        		$io->success('Command exited cleanly!');
+    			
+			 
+        	   }
+
+
+
+
+
+
+
+
+
+				return str_rot13($value);
 	}
 }
