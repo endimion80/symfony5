4142cd598ec6cfea9859e98e360900f7826c07f8
Luis R. Bernal Torres <luisrafaelbernaltorres@Maclaus2.local>
2019-12-07 17:40:30 +0100

    modifiying csv reader

--- rest_api_project/src/Util/ReaderCsv.php
+++ rest_api_project/src/Util/ReaderCsv.php
@@ -1,6 +1,12 @@
 <?php
 
 namespace App\Util;
+use App\Entity\QuestionList;
+use App\Entity\Question;
+use App\Entity\Choice;
+use Doctrine\ORM\EntityManagerInterface;
+use League\Csv\Reader;
+
 //needs adding the libraries  wich permits charging the csv files
 // parameters wich will use are the file name 
 
