9d9492f618371ccd67cb3a478fad1417c847e80d
Luis R. Bernal Torres <luisrafaelbernaltorres@Maclaus2.local>
2019-12-07 16:26:11 +0100

    implementing the service in the contrller

--- rest_api_project/src/Controller/UsingApiController.php
+++ rest_api_project/src/Controller/UsingApiController.php
@@ -5,6 +5,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+//implementing when package Google Translate PHP
+// could be loadedwith instruction 'composer require stichoza/google-translate-php'
+// use Stichoza\GoogleTranslate\GoogleTranslate;
+
+
 Use App\Services\clientReaderCsv;
  
 Use App\services\clientReaderJsons;
@@ -15,10 +20,17 @@ class UsingApiController extends AbstractController
     /*private $customerRepository;
 
     /**
-     * @Route("/question/", name="question_get", methods={"GET"})
+     * @Route("/question/{lang}", name="question_get", methods={"GET"})
      */
-    public function readcsv(Request $request): JsonResponse
+    public function readcsv(Request $request, $lang): JsonResponse
     {
+        //$tr = new GoogleTranslate('en'); 
+        //$tr->setSource(); // Detect language automatically
+        //$tr->setTarget($lang); // Translate to Georgian
+
+        //$translatedstring=GoogleTranslate::trans('Hello again', 'en', $lang);   
+
+
         $data = json_decode($request->getContent(), true);
 
         $firstName = $data['firstName'];
